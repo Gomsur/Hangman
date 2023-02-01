06a190762ba68b48a3ff71f8781d8ae7e5a28d4b
Gomsur <robert@eddiebengtsson.se>
2023-02-01 15:04:31 +0000

    Added a category funtion to the game

--- run.py
+++ run.py
@@ -1,3 +1,25 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+import random
+from animals import word_list as animals
+from country import word_list as country
+from food import word_list as food
+
+class gamesetting:
+    """
+    Choose game category
+    """
+    def __init__(self, lives, words):
+        self.lives = lives
+        self.hidden = ""
+        if word == animals
+        self.animals()
+        else if == "country":
+        self.country()
+        else:
+        self.food()
+
+
+def get_animal

fd602e6d8aebe4e8d42cf648406830de093493bc
Gomsur <robert@eddiebengtsson.se>
2023-02-01 11:23:38 +0000

    Added more countries to the python country list

--- country.py
+++ country.py
@@ -1,22 +1,30 @@
 word_list = [
-    'sweden'
-    'norway'
-    'finland'
-    'denmark'
-    'russia'
-    'german'
-    'spain'
-    'france'
-    'greece'
-    'turkey'
-    'italy'
-    'japan'
-    'china'
-    'argentina'
-    'brazil'
-    'india'
-    'madagaskar'
-    'mexico'
+    'sweden',
+    'norway',
+    'finland',
+    'denmark',
+    'russia',
+    'german',
+    'spain',
+    'france',
+    'greece',
+    'turkey',
+    'italy',
+    'japan',
+    'china',
+    'argentina',
+    'brazil',
+    'india',
+    'madagaskar',
+    'mexico',
+    'albania',
+    'andorra',
+    'bahamas',
+    'barbados',
+    'brazil',
+    'chile',
+    'colombia',
+    'iceland',
     
-
+    
 ]

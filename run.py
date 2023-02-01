d38c02a24149db25e12486d78585b8cf287939d1
Gomsur <robert@eddiebengtsson.se>
2023-02-01 15:46:25 +0000

    Added country defintion so that you are able to pick a word based on the list

--- run.py
+++ run.py
@@ -3,9 +3,9 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
 import random
-from animals import word_list as animals
-from country import word_list as country
-from food import word_list as food
+from animals import word_list as animals_word
+from country import word_list as country_word
+from food import word_list as food_word
 
 class gamesetting:
     """
@@ -14,8 +14,8 @@ class gamesetting:
     def __init__(self, lives, words):
         self.lives = lives
         self.hidden = ""
-        if word == animals
-        self.animals()
+        if word == "animals":
+            self.animals()
         else if == "country":
         self.country()
         else:
@@ -25,7 +25,19 @@ class gamesetting:
         """
         Gets a random word from animals.py and returns it in capitalized letters
         """
-        self.word = (random.choice animals).upper()
+        self.word = random.choice(animals_word).upper()
+        self.hidden = "_" * len(self.word)
 
+    def food(self):
+        """
+        Gets a random word from food.py and returns it in capitalized letters
+        """
+        self.word = random.choice(food_word).upper()
+        self.hidden = "_" * len(self.word)
 
-def get_animal
+    def country(self):
+        """
+        Gets a random word from country.py and returns it in capitalizede letters
+        """
+        self.word = random.choice(country_word).upper()
+        self.hidden = "_" * len(self.word)    

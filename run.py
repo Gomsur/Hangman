0920237549e7ce932b0fa064587b23ac4945c879
Gomsur <robert@eddiebengtsson.se>
2023-02-01 16:02:15 +0000

    Added a logo

--- run.py
+++ run.py
@@ -55,5 +55,16 @@ def intro_logo():
     Intro logo shows up when you start the game
     """
     print(
-
+        text_colors.BLUE
+        + """ Welcome to
+  _    _
+ | |  | |
+ | |__| | __ _ _ __   __ _ _ __ ___   __ _ _ __ 
+ |  __  |/ _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+ | |  | | (_| | | | | (_| | | | | | | (_| | | | |
+ |_|  |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                      __/ |
+                     |___/
+"""
+        + text_colors.WHITE
     )                 
